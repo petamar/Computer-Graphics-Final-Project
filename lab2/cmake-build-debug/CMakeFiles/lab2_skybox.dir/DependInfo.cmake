f3c7b30f3fb3de046570cee2c57a838d08701ded
Petra Marcokova <petra.marcokova@uni-konstanz.de>
2024-12-01 20:02:21 +0100

    Start of final project

---
+++ lab2/cmake-build-debug/CMakeFiles/lab2_skybox.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/petam/Desktop/trinity4/Computer Graphics/The Final Project/lab2/lab2/lab2_skybox.cpp" "CMakeFiles/lab2_skybox.dir/lab2/lab2_skybox.cpp.obj" "gcc" "CMakeFiles/lab2_skybox.dir/lab2/lab2_skybox.cpp.obj.d"
+  "C:/Users/petam/Desktop/trinity4/Computer Graphics/The Final Project/lab2/lab2/render/shader.cpp" "CMakeFiles/lab2_skybox.dir/lab2/render/shader.cpp.obj" "gcc" "CMakeFiles/lab2_skybox.dir/lab2/render/shader.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/lab2/lab2_skybox.h
@@ -0,0 +1,16 @@
+//
+// Created by petam on 23/10/2024.
+//
+
+#ifndef LAB2_SKYBOX_H
+#define LAB2_SKYBOX_H
+
+
+
+class lab2_skybox {
+
+};
+
+
+
+#endif //LAB2_SKYBOX_H
