3093303cc8dc5184e3f976552f9ad5eca217d58b
Petra Marcokova <petra.marcokova@uni-konstanz.de>
2024-12-13 22:59:25 +0100

    Added buildings and skybox

--- lab2/lab2/lab2_building.cpp
+++ lab2/lab2/lab2_building.cpp
@@ -28,15 +28,13 @@ static float viewPolar = 0.f;
 static float viewDistance = 300.0f;
 
 const char* textureFiles[] = {
-	"../lab2/facade0.jpg",
-	"../lab2/facade1.jpg",
-	"../lab2/facade3.jpg",
-	"../lab2/facade4.jpg",
+	"../lab2/textures/fasade_sky.png",
+	"../lab2/textures/fasade_sky2.png",
+	"../lab2/textures/sky1.png",
 };
-const int numTextures = sizeof(textureFiles) / sizeof(textureFiles[0]);
-
 
 static GLuint LoadTextureTileBox(const char *texture_file_path) {
+
     int w, h, channels;
     uint8_t* img = stbi_load(texture_file_path, &w, &h, &channels, 3);
     GLuint texture;
@@ -207,7 +205,7 @@ struct Building {
 	GLuint textureSamplerID;
 	GLuint programID;
 
-	void initialize(glm::vec3 position, glm::vec3 scale) {
+	void initialize(glm::vec3 position, glm::vec3 scale, int numOfTexture) {
 		// Define scale of the building geometry
 		this->position = position;
 		this->scale = scale;
@@ -251,8 +249,259 @@ struct Building {
 		// Get a handle for our "MVP" uniform
 		mvpMatrixID = glGetUniformLocation(programID, "MVP");
 
-		// Randomly choose a texture file path
-		const char* selectedTexture = textureFiles[rand() % numTextures];
+		const char* selectedTexture = textureFiles[numOfTexture];
+		textureID = LoadTextureTileBox(selectedTexture);
+
+		// Set up the shader and get the texture sampler uniform
+		textureSamplerID = glGetUniformLocation(programID, "textureSampler");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+
+		// Model transform
+        glm::mat4 modelMatrix = glm::mat4(1.0f);
+		modelMatrix = glm::translate(modelMatrix, position);
+        modelMatrix = glm::scale(modelMatrix, scale);
+
+		// Set model-view-projection matrix
+		glm::mat4 mvp = cameraMatrix * modelMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+		// TODO: Enable UV buffer and texture sampler
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glUniform1i(textureSamplerID, 0);
+
+		// Draw the box
+		glDrawElements(
+			GL_TRIANGLES,      // mode
+			36,    			   // number of indices
+			GL_UNSIGNED_INT,   // type
+			(void*)0           // element array buffer offset
+		);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+        //glDisableVertexAttribArray(2);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteBuffers(1, &indexBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteProgram(programID);
+	}
+};
+
+
+struct Skyboxik {
+	glm::vec3 position;
+	glm::vec3 scale;
+
+	GLfloat vertex_buffer_data[72] = {
+		// Front face
+		-1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+
+		// Back face
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+
+		// Left face
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+
+		// Right face
+		1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Top face
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+
+		// Bottom face
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+	};
+
+	GLfloat color_buffer_data[72] = {
+		// Front, red
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Back, yellow
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Left, green
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Right, cyan
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Top, blue
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+
+		// Bottom, magenta
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+	};
+
+	GLuint index_buffer_data[36] = {		// 12 triangle faces of a box
+		0, 1, 2,
+		0, 2, 3,
+
+		4, 5, 6,
+		4, 6, 7,
+
+		8, 9, 10,
+		8, 10, 11,
+
+		12, 13, 14,
+		12, 14, 15,
+
+		16, 17, 18,
+		16, 18, 19,
+
+		20, 21, 22,
+		20, 22, 23,
+	};
+
+	GLfloat uv_buffer_data[48] = {
+		// Front
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		1.0f, 0.0f,
+		0.0f, 0.0f,
+		// Back
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		 1.0f, 0.0f,
+		 0.0f, 0.0f,
+		 // Left
+		 0.0f, 1.0f,
+		 1.0f, 1.0f,
+		 1.0f, 0.0f,
+		 0.0f, 0.0f,
+		 // Right
+		 0.0f, 1.0f,
+		 1.0f, 1.0f,
+		 1.0f, 0.0f,
+		 0.0f, 0.0f,
+		 // Top - we do not want texture the top
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+		 // Bottom - we do not want texture the bottom
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+		 0.0f, 0.0f,
+        };
+
+
+	// OpenGL buffers
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint indexBufferID;
+	GLuint colorBufferID;
+	GLuint uvBufferID;
+	GLuint textureID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint textureSamplerID;
+	GLuint programID;
+
+	void initialize(glm::vec3 position, glm::vec3 scale, int numOfTexture) {
+		// Define scale of the building geometry
+		this->position = position;
+		this->scale = scale;
+
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		//for (int i = 0; i < 72; ++i) color_buffer_data[i] = 1.0f;
+
+		//code to make the texture repeat
+		//for (int i = 0; i < 24; ++i) uv_buffer_data[2*i+1] *= 5;
+
+
+		// Create a vertex buffer object to store the vertex data
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		glGenBuffers(1, &uvBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
+
+		// Create an index buffer object to store the index data that defines triangle faces
+		glGenBuffers(1, &indexBufferID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_buffer_data), index_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../lab2/box.vert", "../lab2/box.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+
+		const char* selectedTexture = textureFiles[numOfTexture];
 		textureID = LoadTextureTileBox(selectedTexture);
 
 		// Set up the shader and get the texture sampler uniform
@@ -846,6 +1095,37 @@ struct Spire {
 
 
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+	static float lastX = 400, lastY = 300; // Initialize with center of screen
+	static bool firstMouse = true;
+
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xOffset = xpos - lastX;
+	float yOffset = lastY - ypos; // Reversed: y-coordinates range bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // Adjust sensitivity
+	xOffset *= sensitivity;
+	yOffset *= sensitivity;
+
+	viewAzimuth += glm::radians(xOffset);
+	viewPolar += glm::radians(yOffset);
+
+	// Constrain polar angle to prevent flipping
+	if (viewPolar > glm::radians(89.0f)) viewPolar = glm::radians(89.0f);
+	if (viewPolar < glm::radians(-89.0f)) viewPolar = glm::radians(-89.0f);
+}
+
+
+
+
+
 int main(void)
 {
 	// Initialise GLFW
@@ -888,6 +1168,10 @@ int main(void)
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
+	//mouse callback
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	//PLATEAU SET UP
 	//_______________________________________________________________________________________________________________
 	Plateau plateau;
@@ -900,23 +1184,39 @@ int main(void)
 	//BUILDING SET UP
 	//___________________________________________________________________________________________________________________
 
-	std::vector<Building> buildings;
-	srand(static_cast<unsigned int>(time(0)));
-	for (int i = 0; i < 1; i++) {
-		for (int j = 0; j < 1; j++) {
-			Building b;
+	Building building1;
+	building1.initialize(glm::vec3(150, 10, 20), glm::vec3(50, 300, 70),0);
 
-			// Generate a random height for the building
-			float randomHeight = 20.0f + static_cast<float>(rand() % 75);
+	Building building2;
+	building2.initialize(glm::vec3(150, 10, 200), glm::vec3(50, 300, 70),1);
 
-			glm::vec3 scale(16.0f, randomHeight, 16.0f); // Random height, fixed width and depth
+	Building building3;
+	building3.initialize(glm::vec3(150, 10, 400), glm::vec3(50, 300, 70),0);
 
-			glm::vec3 position(i * 50, randomHeight - 20.0f , j * 50);
 
-			b.initialize(position, scale);
-			buildings.push_back(b);
-		}
-	}
+	Building building4;
+	building4.initialize(glm::vec3(150, 10, -180), glm::vec3(50, 300, 70),0);
+
+
+	Building building5;
+	building5.initialize(glm::vec3(150, 10, -380), glm::vec3(50, 300, 70),0);
+
+	Building building6;
+	building6.initialize(glm::vec3(-150, 10, 20), glm::vec3(50, 300, 70),0);
+
+	Building building7;
+	building7.initialize(glm::vec3(-150, 10, 200), glm::vec3(50, 300, 70),0);
+
+	Building building8;
+	building8.initialize(glm::vec3(-150, 10, 400), glm::vec3(50, 300, 70),0);
+
+
+	Building building9;
+	building9.initialize(glm::vec3(-150, 10, -180), glm::vec3(50, 300, 70),0);
+
+
+	Skyboxik skybox;
+	skybox.initialize(glm::vec3(0, 0, -0), glm::vec3(500, 500, 500),2);
 
 	//------------------------------------------------------------------------------------------------------------------
 	// Camera setup
@@ -939,28 +1239,41 @@ int main(void)
 		glm::mat4 vp = projectionMatrix * viewMatrix;
 
 		plateau.render(vp);
-
 		spire.render(vp);
-
-		// Render all buildings
-		for (Building &b : buildings) {
-			//b.render(vp);
-		}
+		building1.render(vp);
+		building2.render(vp);
+		building1.render(vp);
+		building3.render(vp);
+		building4.render(vp);
+		building5.render(vp);
+		building6.render(vp);
+		building7.render(vp);
+		building8.render(vp);
+		building9.render(vp);
+		skybox.render(vp);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
 
 	} // Check if the ESC key was pressed or the window was closed
 	while (!glfwWindowShouldClose(window));
 
 	// CLEAN UP
 	// ------------------------------------------------------------------------------------------------------
-	for (Building &b : buildings) {
-		b.cleanup();
-	}
-	plateau.cleanup();
+	building1.cleanup();
+	building2.cleanup();
+	building3.cleanup();
+	building4.cleanup();
+	building5.cleanup();
+	building6.cleanup();
+	building7.cleanup();
+	building8.cleanup();
+	building9.cleanup();
+	skybox.cleanup();
 
+	plateau.cleanup();
 	spire.cleanup();
 	//-----------------------------------------------------------------------------------------------------
 
@@ -1003,9 +1316,6 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 	}
 
 
-
-
-
 	if (key == GLFW_KEY_P && (action == GLFW_REPEAT || action == GLFW_PRESS))
 	{
 		// Zoom in by reducing the distance from the object
@@ -1030,6 +1340,36 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 		eye_center.z = viewDistance * sin(viewAzimuth) * sin(viewPolar);
 	}
 
+
+	if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		float speed = 1.0f; // Adjust speed as needed
+
+		// Compute forward direction based on azimuth and polar angles
+		glm::vec3 forwardDirection = glm::vec3(
+			cos(viewPolar) * sin(viewAzimuth), // x
+			sin(viewPolar),                    // y
+			cos(viewPolar) * cos(viewAzimuth)  // z
+		);
+
+		// Move the lookat and eye_center forward
+		lookat += forwardDirection * speed;
+		eye_center += forwardDirection * speed;
+	}
+
+	if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		float speed = 1.0f; // Adjust speed as needed
+
+		// Compute backward direction
+		glm::vec3 backwardDirection = glm::vec3(
+			-cos(viewPolar) * sin(viewAzimuth), // x
+			-sin(viewPolar),                    // y
+			-cos(viewPolar) * cos(viewAzimuth)  // z
+		);
+
+		// Move the lookat and eye_center backward
+		lookat += backwardDirection * speed;
+		eye_center += backwardDirection * speed;
+	}
 
 
 	// Update the camera's position based on the updated viewDistance
